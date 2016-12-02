7bdd25333b89461804d5e9cf6c76bb021e93a1f1
MohammedKhan786 <khanm168@uni.coventry.ac.uk>
2016-12-02 20:38:06 +0000

    Update Task 10  ascending order.py

--- Task 10  ascending order.py
+++ Task 10  ascending order.py
@@ -1,3 +1,7 @@
+#Given a sequence of n integer numbers, extract the sub-sequence of maximum length which is in
+#ascending order. Example input: L = [1,2,3,4,1,5,1,6,7] Output: [1,2,3,4]
+
+
 def order(L):
     L.append (0)
     Csorting = []
@@ -7,15 +11,18 @@ def order(L):
 
     for i in range(0,len(L)-1):
         number = (L[i])
+        # if [i] is bigger then [i+1] then add one to Ccount and add L[i] to Csorting and repeat until [i] is less or equal to [i+1]
         if L[i] < L[i+1]:
             Ccount = Ccount + 1
             Csorting.append (number)
+        # if [i] is less or equal to [i+1] and Ccount is bigger Lcount replace Lcount with Ccount and Lsorted with Csorting
         if L[i] >= L[i+1] and Ccount > Lcount:
             Csorting.append (number)
             Lcount = Ccount
             Ccount = 1
             Lsorted=Csorting
             Csorting=[]
+         # if [i] is less or equal to [i+1] and Ccount is less then Lcount reset Ccount and Csorting
         if L[i] >= L[i+1] and Ccount < Lcount:
              Ccount = 1
              Csorting=[]
