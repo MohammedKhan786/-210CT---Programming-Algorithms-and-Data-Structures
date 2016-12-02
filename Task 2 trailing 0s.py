f0f1c9fd549ff5fad7b54da31ea7270d68a3d64e
MohammedKhan786 <khanm168@uni.coventry.ac.uk>
2016-12-02 18:51:33 +0000

    Update Task 2 trailing 0s.py

--- Task 2 trailing 0s.py
+++ Task 2 trailing 0s.py
@@ -1,5 +1,8 @@
+#Count the number of trailing 0s in a factorial number. 
+
 def trailing(number):
     trailing = 0
+    #divide by 5 and add up the answer untill the answer is 1
     while number > 1:
         number = number // 5
         trailing = number + trailing
