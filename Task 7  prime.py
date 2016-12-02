992a6199e3092ed2ef6da31934d4a3807da91033
MohammedKhan786 <khanm168@uni.coventry.ac.uk>
2016-12-02 20:59:37 +0000

    Update Task 7  prime.py

--- Task 7  prime.py
+++ Task 7  prime.py
@@ -1,3 +1,13 @@
+def primecheck (N):
+    if N == 2:
+        return True
+    elif N < 2 or N%2 == 0:
+        return False
+    else:
+        check = N - 1
+        prime(N, check)
+
+
 def prime (N, check):
     remainder = N % check
     if remainder ==0:
@@ -7,15 +17,3 @@ def prime (N, check):
     else:
         check = check -1
         prime (N, check)
-        
-    
-
-def primecheck (N):
-    if N == 2:
-        return True
-    elif N < 2 or N%2 == 0:
-        return False
-    else:
-        check = N - 1
-        prime(N, check)
-        
