2d2c499f281feab8b69a0fa707a592b1c8fc01ae
MohammedKhan786 <khanm168@uni.coventry.ac.uk>
2016-12-02 19:11:37 +0000

    Update Task 6 reverses.py

--- Task 6 reverses.py
+++ Task 6 reverses.py
@@ -1,6 +1,9 @@
+#Write the pseudocode and code for a function that reverses the words in a sentence. Input:
+#"This is awesome" Output: "awesome is This". Give the Big O notation.
+    
 LReverse=[]
 
-
+#remove the last word and add it to the reverses list 
 def reverse(I):
     L = I.split()
     while len(L) > 0:
@@ -9,5 +12,3 @@ def reverse(I):
         LReverse.append(num)
         if len(L) == 0:
             return LReverse
-
-
