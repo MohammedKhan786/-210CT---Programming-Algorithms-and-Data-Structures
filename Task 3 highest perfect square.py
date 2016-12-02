022d58788155f787dbe662105565776027663f31
MohammedKhan786 <khanm168@uni.coventry.ac.uk>
2016-12-02 19:00:59 +0000

    Update Task 3 highest perfect square.py

--- Task 3 highest perfect square.py
+++ Task 3 highest perfect square.py
@@ -4,12 +4,13 @@
 def perfectsquare(Number):
     N = 0
     square = 0
-
+    
+#add one to N until N^2 is bigger then the input number
     while Number >= square:
         N = N + 1
         square = N * N
         
-        
+#take one away for a N to find the biggest square number under the number        
     N = N - 1
     square = N * N
     return (square)
