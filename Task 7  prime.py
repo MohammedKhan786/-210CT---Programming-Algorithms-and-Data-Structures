7eebe8cc80bd35103c045fed5c4e95c4d2f9cef5
MohammedKhan786 <khanm168@uni.coventry.ac.uk>
2016-12-02 21:33:39 +0000

    Update Task 7  prime.py

--- Task 7  prime.py
+++ Task 7  prime.py
@@ -1,8 +1,16 @@
+#Write a recursive function (pseudocode and code) to check if a number n is prime (hint:
+#check whether n is divisible by any number below n).
+
+
 def primecheck (N):
+    # check in N 2 and if it is return true
     if N == 2:
         return True
+    # check if N is less then 2 because if n is less the 2 it can not be a prime
+    # check if there a remainder when dividing by 2 
     elif N < 2 or N%2 == 0:
         return False
+    #take 1 away from N and call prime
     else:
         check = N - 1
         prime(N, check)
@@ -10,10 +18,13 @@ def primecheck (N):
 
 def prime (N, check):
     remainder = N % check
+    # if remainder = 0 that mean that N can be dividing by another number with mean it on a prime
     if remainder ==0:
         print (False)
+    # if check <= 2 then it has check all the number belowe N to check if there was a remainder
     elif check <= 2:
         print (True)
+     #take 1 away from N and call prime
     else:
         check = check -1
         prime (N, check)
